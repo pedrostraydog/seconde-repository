18a17d912c55777832ff691deb540b6235c1d540
Pedro Straydog <tlilibadreddine@gmail.com>
2024-02-14 18:29:02 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo2.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Share Love
+        </h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
